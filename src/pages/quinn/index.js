08b6196fdb8fe7668c05034d98b8fa3964f1c017
Quinnlan Hipp <99678158+quinnhipp@users.noreply.github.com>
2023-01-14 00:06:55 -0500

    Init

--- src/pages/quinn/index.js
+++ src/pages/quinn/index.js
@@ -5,5 +5,5 @@ import { Inter } from "@next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <>Hello World!</>;
+  return <>Brian is cool</>;
 }
